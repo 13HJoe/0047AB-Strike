e929489df63d51866f590babacec3e3e40417e2f
Joseph Merigala <joseph.merigala@gmail.com>
2024-11-04 11:43:15 +0530

    cmdhist js

--- C2_Django/UI/static/js/source.js
+++ C2_Django/UI/static/js/source.js
@@ -1,1 +1,13 @@
-document.addEventListener("DOMContentLoaded")
+function gethistory(){
+    let ip = document.getElementById('ip').innerHTML
+    fetch(`http://127.0.0.1:8000/exec_hist?ip=${ip}`)
+    .then(response =>{
+        return response.text();
+    })
+    .then(data => {
+        for(i=0; i<data.length(); i++){
+            
+        }
+        document.querySelector('.resp').innerHTML = data;
+    })
+}3
